12f20bd7a841df8b40844681888e0fbff822c31a
youswim <youswim96@gmail.com>
2022-07-25 23:53:51 +0900

    49

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,10 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+
+@ServletComponentScan
 @SpringBootApplication
 public class ServletApplication {
 
---
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,42 @@
+package hello.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println("[전체 파라미터 조회] - start");
+
+        req.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> System.out.println("paramName = "
+                        + req.getParameter(paramName)));
+        System.out.println("[전체 파라미터 조회] - end");
+
+        System.out.println();
+
+        System.out.println("[단일 파라미터 조회]");
+        String username = req.getParameter("username");
+        System.out.println("request.getParameter(username) = " + username);
+
+        String age = req.getParameter("age");
+        System.out.println("request.getParameter(age) = " + age);
+        System.out.println();
+
+        System.out.println("[이름이 같은 복수 파라미터 조회]");
+        System.out.println("request.getParameterValues(username)");
+        String[] usernames = req.getParameterValues("username");
+        for (String name : usernames) {
+            System.out.println("username=" + name);
+        }
+
+
+    }
+}
